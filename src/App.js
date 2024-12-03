2e23f568cadd178666b51a16dd7006a9a904d465
temitopejoshua <iamoyemadeteepee@gmail.com>
2024-12-02 20:49:01 -0500

    updated changes

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       <h2>Welcome to Database design class</h2>
       <h3>Temitope Oyemade</h3>
       <a href='mailto:OYEMADET0226@students.bowiestate.edu' target='_blank' rel="noreferrer">OYEMADET0226@students.bowiestate.edu</a>
+      <br/>
       <a href='https://www.linkedin.com/in/temitope-oyemade-94552a179/' target='_blank' rel="noreferrer">Linkedin</a>
       <br/>
       <a href='https://github.com/temitopejoshua' target='_blank' rel="noreferrer">Github</a>
